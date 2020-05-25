8b701d67ba0f87f23c438247dfba4166bb7c0765
smanicome <s.manicome@gmail.com>
2020-05-25 16:14:52 +0200

    ProgWeb TP4 Done

--- ProgWeb/TP4/Memory/memory.js
+++ ProgWeb/TP4/Memory/memory.js
@@ -1,32 +1,80 @@
 
 function MemoryGame(images, blank) {
-  this.images = images;
-  this.blank = blank;
+	this.images = images;
+	this.blank = blank;
 }
 
 MemoryGame.prototype.build = function build(div) {
-  var cards = shuffleCards(this.images.length);
-  var nodes = cards.map((c) => {
-    var node = document.createElement("img");
-    node.src = "blank.png";
-    node.alt = "blank";
-    node.onclick = function(ev) {
-      node.src = "lego" + (c+1) + ".png";
-      node.alt = "lego" + (c+1);
-    };
-    return node;
-  });
+	var cards = shuffleCards(this.images.length);
+	var discovered = cards.map(c => false);
+	var flipped = 0;
+	var flippedCards = [];
 
-  nodes.forEach( function(n) {
-    div.appendChild(n);
-  });
+
+	var nodes = cards.map((c, index) => {
+		var node = document.createElement("img");
+		node.src = "blank.png";
+		node.alt = "blank";
+		return node;
+	});
+
+	// separated from above to modify nodes on click
+	for (var i = 0; i < nodes.length; i++) {
+		var node = nodes[i];
+		node.onclick = (function(i){
+			return function() {
+				if (discovered[i]) return;
+
+				console.log(i);
+				nodes[i].src = "lego" + (cards[i]+1) + ".png";
+				nodes[i].alt = "lego" + (cards[i]+1);
+
+				discovered[i] = true
+				flippedCards.push(cards[i]);
+				flipped++;
+
+
+				if (flipped === 2) {
+					if (flippedCards.some((card) => card !== cards[i])) {
+						setTimeout((flippedCards) => {
+							flippedCards.forEach( function(card) {
+								var idx = cards.indexOf(card);
+								discovered[idx] = false;
+								nodes[idx].src = "blank.png";
+								nodes[idx].alt = "blank"
+
+								idx = cards.lastIndexOf(card);
+								discovered[idx] = false;
+								nodes[idx].src = "blank.png";
+								nodes[idx].alt = "blank";
+							});
+
+						}, 500, flippedCards);
+					}
+					flipped = 0;
+					flippedCards = [];
+				}
+			};
+		})(i);
+
+		div.appendChild(node);
+	}
 }
-// '<img src="lego' + (c+1) + '.png" alt="lego' + (c+1) +'"/>'
+
 function shuffleCards(length) {
-  var cards = [];
-  for(var i = 0; i < length; i++) {
-    cards.push(i);
-    cards.push(i);
-  }
-  return cards;
+	var cards = [];
+	for(let i = 0; i < length; i++) {
+		cards.push(i);
+		cards.push(i);
+	}
+
+	var j, x;
+	for (let i = cards.length - 1; i > 0; i--) {
+		j = Math.floor(Math.random() * (i + 1));
+		x = cards[i];
+		cards[i] = cards[j];
+		cards[j] = x;
+	}
+
+	return cards;
 }
