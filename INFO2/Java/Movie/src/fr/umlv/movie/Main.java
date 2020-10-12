476490e2a9145d8c9e92b7e888ece87a2e113b0b
smanicome <43649039+smanicome@users.noreply.github.com>
2020-10-12 20:36:28 +0200

    Java Avancée Movie

--- INFO2/Java/Movie/src/fr/umlv/movie/Main.java
+++ INFO2/Java/Movie/src/fr/umlv/movie/Main.java
@@ -6,16 +6,13 @@ import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RuntimeException{
         Path path = Path.of("movies.txt");
-
+        
         try(Stream<String> stream = Files.lines(path)) {
             stream.forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("Reading went wrong");
-            e.printStackTrace();
+        	throw new RuntimeException(e);
         }
     }
-    
-    
 }
